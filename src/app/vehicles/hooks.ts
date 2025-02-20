ad11ff41f360f62ff3b7b33d6482cdad9e3d4197
Dixy <dixyxavier@gmail.com>
2025-02-20 20:08:43 +0100

    Adds code refactoring

--- src/app/vehicles/hooks.ts
+++ src/app/vehicles/hooks.ts
@@ -1,7 +1,8 @@
 import { Vehicle } from "@/types";
 import { LIMIT } from "@/utils/contants";
+import { debounce } from "@/utils/general";
 import { getVehicles } from "@/utils/services";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 export const useVehicles = (page: number, sort: boolean, filterText: string) => {
     const [state, setState] = useState({
@@ -24,6 +25,37 @@ export const useVehicles = (page: number, sort: boolean, filterText: string) => 
         fetchVehicles();
     }, [fetchVehicles]);
 
-    return { ...state, fetchVehicles };
+    return useMemo(() => ({ ...state, fetchVehicles }), [state, fetchVehicles]);
 };
 
+export const useVehicleListActions = (
+    page: number,
+    setPage: React.Dispatch<React.SetStateAction<number>>,
+    setSort: React.Dispatch<React.SetStateAction<boolean>>,
+    setFilterText: React.Dispatch<React.SetStateAction<string>>,
+    totalPages: number,
+    vehicles: Vehicle[]
+) => {
+    const toggleSort = useCallback(() => {
+        setSort(prevSort => !prevSort);
+    }, [setSort]);
+
+    const debounceFilter = useMemo(() => debounce((filterText: string) => {
+        setPage(1);
+        setFilterText(filterText);
+    }, 300), [setPage, setFilterText]);
+
+    const loadPrevious = useCallback(() => {
+        if (page > 1) {
+            setPage(prevPage => prevPage - 1);
+        }
+    }, [page, setPage]);
+
+    const loadMore = useCallback(() => {
+        if (page < totalPages && vehicles.length >= LIMIT) {
+            setPage(prevPage => prevPage + 1);
+        }
+    }, [page, totalPages, vehicles.length, setPage]);
+
+    return { toggleSort, debounceFilter, loadPrevious, loadMore };
+};
--- src/app/vehicles/page.tsx
+++ src/app/vehicles/page.tsx
@@ -1,68 +1,54 @@
 'use client';
-import React, { useState, useCallback, useMemo } from 'react';
+import React, { useState, useCallback } from 'react';
 import Link from 'next/link';
-import { useVehicles } from './hooks';
-import { debounce } from '@/utils/general';
-import { LIMIT } from '@/utils/contants';
+import { useVehicleListActions, useVehicles } from './hooks';
 
 const VehicleList = () => {
     const [page, setPage] = useState(1);
     const [sort, setSort] = useState(false);
     const [filterText, setFilterText] = useState('');
     const { vehicles, totalPages, loading } = useVehicles(page, sort, filterText);
+    const { toggleSort, debounceFilter, loadPrevious, loadMore } = useVehicleListActions(page, setPage, setSort, setFilterText, totalPages, vehicles);
 
-    const toggleSort = useCallback(() => {
-        setSort(prevSort => !prevSort);
-    }, []);
-
-    const debounceFilter = useMemo(() => debounce((filterText: string) => {
-        setPage(1);
-        setFilterText(filterText);
-    }, 300), []);
-
-    const loadPrevious = useCallback(() => {
-        if (page > 1) {
-            setPage(prevPage => prevPage - 1);
-        }
-    }, [page]);
-
-    const loadMore = useCallback(() => {
-        if (page < totalPages && vehicles.length >= LIMIT) {
-            setPage(prevPage => prevPage + 1);
-        }
-    }, [page, totalPages]);
+    const handleFilterChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        const filterText = e.target.value.toLowerCase();
+        debounceFilter(filterText);
+    }, [debounceFilter]);
 
     return (
         <div>
             <h1>Vehicle List</h1>
-            {vehicles.length === 0 && loading ? <p>Loading...</p> : (
+            {loading && vehicles.length === 0 ? (
+                <p>Loading...</p>
+            ) : (
                 <div>
                     <div>
                         <input
                             type="text"
                             placeholder="Filter by brand or model"
-                            onChange={(e) => {
-                                const filterText = e.target.value.toLowerCase();
-                                debounceFilter(filterText);
-                            }}
+                            onChange={handleFilterChange}
                         />
                     </div>
-                    {!loading && vehicles.length > 0 && <div>
-                        <button onClick={toggleSort}>{sort ? 'Unsort' : 'Sort by brand'}</button>
-                    </div>}
+                    {!loading && vehicles.length > 0 && (
+                        <div>
+                            <button onClick={toggleSort}>{sort ? 'Unsort' : 'Sort by brand'}</button>
+                        </div>
+                    )}
                     <ul>
-                        {vehicles.map(vehicle => (
-                            <li key={vehicle.id}>
-                                <Link href={`/vehicles/${vehicle.id}`}>
-                                    {vehicle.brand} {vehicle.model} {vehicle.year}
+                        {vehicles.map(({ id, brand, model, year }) => (
+                            <li key={id}>
+                                <Link href={`/vehicles/${id}`}>
+                                    {brand} {model} {year}
                                 </Link>
                             </li>
                         ))}
                     </ul>
-                    {!loading && vehicles.length > 0 && <div>
-                        <button onClick={loadPrevious}>Load previous</button>
-                        <button onClick={loadMore}>Load more</button>
-                    </div>}
+                    {!loading && vehicles.length > 0 && (
+                        <div>
+                            <button onClick={loadPrevious}>Load previous</button>
+                            <button onClick={loadMore}>Load more</button>
+                        </div>
+                    )}
                 </div>
             )}
             {!loading && vehicles.length === 0 && <p>No vehicles found</p>}
